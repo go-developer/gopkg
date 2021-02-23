99dc9b8b69a9c0a3eaa5c6e5b9dc1f74c14da1e6
张德满 <go_developer@163.com>
2021-02-23 22:49:06 +0800

    add segment 待完成

---
+++ easymap/segment.go
@@ -0,0 +1,11 @@
+// Package easymap...
+//
+// Description : 分段存储的map，并发行更好,分段数量为 1， 将退化成普通的
+//
+// Author : go_developer@163.com<张德满>
+//
+// Date : 2021-02-23 10:47 下午
+package easymap
+
+type segment struct {
+}
