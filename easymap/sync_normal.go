af41b19e51090bf8a2562178e176eaa22ce38333
张德满 <go_developer@163.com>
2021-02-24 14:09:58 +0800

    增加sync.Map文件, 待实现

---
+++ easymap/sync_normal.go
@@ -0,0 +1,8 @@
+// Package easymap...
+//
+// Description : 内置的sync.Map
+//
+// Author : go_developer@163.com<张德满>
+//
+// Date : 2021-02-24 2:08 下午
+package easymap
---
+++ easymap/sync_segment.go
@@ -0,0 +1,8 @@
+// Package easymap...
+//
+// Description : 内置sync.Map + segment
+//
+// Author : go_developer@163.com<张德满>
+//
+// Date : 2021-02-24 2:08 下午
+package easymap
