498f3ec7c45e4a88663c838b858f4504cf203946
张德满 <go_developer@163.com>
2021-03-09 20:40:55 +0800

    add gin router group

--- gin/util/responsed.go
+++ gin/util/responsed.go
@@ -61,3 +61,33 @@ func RegisterRouter(router *gin.Engine, method string, uri string, handler gin.H
 	}
 	return nil
 }
+
+// RegisterRouterGroup 注册gin路由
+//
+// Author : go_developer@163.com<张德满>
+//
+// Date : 8:36 下午 2021/3/9
+func RegisterRouterGroup(router *gin.RouterGroup, method string, uri string, handler gin.HandlerFunc) error {
+	switch strings.ToUpper(method) {
+	case http.MethodGet:
+		router.GET(uri, handler)
+	case http.MethodPost:
+		router.POST(uri, handler)
+	case http.MethodDelete:
+		router.DELETE(uri, handler)
+	case http.MethodHead:
+		router.HEAD(uri, handler)
+	case http.MethodOptions:
+		router.OPTIONS(uri, handler)
+	case http.MethodPatch:
+		router.PATCH(uri, handler)
+	case http.MethodPut:
+		router.PUT(uri, handler)
+	case "ANY": // 一次性注册全部请求方法的路由
+		router.Any(uri, handler)
+	default:
+		// 不是一个函数,数名method配置错误
+		return fmt.Errorf("uri=%s method=%s 请求方法配置错误", uri, method)
+	}
+	return nil
+}
