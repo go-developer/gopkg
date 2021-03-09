f4d09801ff956409f49aa5c60b2de843eb0778a0
张德满 <go_developer@163.com>
2021-03-09 20:37:08 +0800

    增加注册gin路由的方法

--- gin/util/responsed.go
+++ gin/util/responsed.go
@@ -8,7 +8,9 @@
 package util
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -28,4 +30,34 @@ func Response(ctx *gin.Context, code interface{}, message string, data interface
 		"cost":     time.Since(time.Unix(ctx.GetInt64("start_time"), 0)).Seconds(),
 	}
 	ctx.JSON(http.StatusOK, responseData)
+}
+
+// RegisterRouter 注册gin路由
+//
+// Author : go_developer@163.com<张德满>
+//
+// Date : 8:36 下午 2021/3/9
+func RegisterRouter(router *gin.Engine, method string, uri string, handler gin.HandlerFunc) error {
+	switch strings.ToUpper(method) {
+	case http.MethodGet:
+		router.GET(uri, handler)
+	case http.MethodPost:
+		router.POST(uri, handler)
+	case http.MethodDelete:
+		router.DELETE(uri, handler)
+	case http.MethodHead:
+		router.HEAD(uri, handler)
+	case http.MethodOptions:
+		router.OPTIONS(uri, handler)
+	case http.MethodPatch:
+		router.PATCH(uri, handler)
+	case http.MethodPut:
+		router.PUT(uri, handler)
+	case "ANY": // 一次性注册全部请求方法的路由
+		router.Any(uri, handler)
+	default:
+		// 不是一个函数,数名method配置错误
+		return fmt.Errorf("uri=%s method=%s 请求方法配置错误", uri, method)
+	}
+	return nil
 }
