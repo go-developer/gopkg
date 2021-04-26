8227b98d148e4e185b768d055920122c27c06ce7
张德满 <go_developer@163.com>
2021-04-26 22:40:26 +0800

    增加判断文件是否存在/读取文件内容/解析yml配置的方法

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/tidwall/gjson v1.6.8
 	github.com/tidwall/pretty v1.1.0 // indirect
 	go.uber.org/zap v1.16.0
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gorm.io/driver/mysql v1.0.4
 	gorm.io/gorm v1.20.12
 )
--- util/file.go
+++ util/file.go
@@ -8,8 +8,12 @@
 package util
 
 import (
+	"io/ioutil"
 	"os"
 	"strings"
+
+	"github.com/pkg/errors"
+	yml "gopkg.in/yaml.v2"
 )
 
 // GetProjectPath 获取项目路径(可执行文件所在目录)
@@ -29,4 +33,55 @@ func GetProjectPath() (string, error) {
 		}
 	}
 	return rootPath, nil
+}
+
+// ReadYmlConfig 读取yml配置问价,并解析到指定的结构体中
+//
+// Author : go_developer@163.com<张德满>
+//
+// Date : 10:35 下午 2021/4/26
+func ReadYmlConfig(filePath string, result interface{}) error {
+	if nil == result {
+		return errors.New("接收读取结果的数据指针为NIL")
+	}
+	var (
+		fileContent []byte
+		err         error
+	)
+	if fileContent, err = ReadFileContent(filePath); nil != err {
+		return err
+	}
+	return yml.Unmarshal(fileContent, result)
+}
+
+// ReadFileContent 读取文件内容
+//
+// Author : go_developer@163.com<张德满>
+//
+// Date : 10:37 下午 2021/4/26
+func ReadFileContent(filePath string) ([]byte, error) {
+	if exist, isFile := IsFileExist(filePath); !exist || !isFile {
+		//文件不存在或者是一个目录
+		return nil, errors.New(filePath + " 文件不存在或者是一个目录!")
+	}
+	//打开文件
+	var (
+		f   *os.File
+		err error
+	)
+	if f, err = os.Open(filePath); nil != err {
+		return nil, err
+	}
+
+	return ioutil.ReadAll(f)
+}
+
+// IsFileExist 判断文件是否存在
+//
+// Author : go_developer@163.com<张德满>
+//
+// Date : 10:37 下午 2021/4/26
+func IsFileExist(filePath string) (bool, bool) {
+	f, err := os.Stat(filePath)
+	return nil == err || os.IsExist(err), (nil == err || os.IsExist(err)) && !f.IsDir()
 }
